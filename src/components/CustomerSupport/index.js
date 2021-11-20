a840e6e88239eecece2e8f4f2e3fd59268f05c9a
Tasya Lailinissa <tasya.lailinissa@gmail.com>
2021-11-20 19:13:07 +0700

    Customer support component

---
+++ src/components/CustomerSupport/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ContactNumber from 'components/ContactNumber';
+
+import './CustomerSupport.scss';
+
+const CustomerSupport = (props) => {
+  const { number } = props;
+
+  return(
+    <div className="customer-support">
+      <h2 className="title">Customer Support</h2>
+      <ContactNumber number={number} />
+      <p className="contact-description">
+        Contact our customer support or email us at <a href="mailto:help@dana.id" class="email"><b>help@dana.id</b></a> if you need help
+      </p>
+    </div>
+  )
+};
+
+ContactNumber.propTypes = {
+  number: PropTypes.string,
+};
+
+ContactNumber.defaultProps = {
+  number: '',
+};
+
+export default CustomerSupport;
