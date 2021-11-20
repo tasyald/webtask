c03288ce981554e59ad6b473462fdb6db0960f3f
Tasya Lailinissa <tasya.lailinissa@gmail.com>
2021-11-20 19:13:48 +0700

    Add props to social media component

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookSquare, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 import './SocialMedia.scss';
 
-const SocialMedia = () => (
-  <div className="social-media">
-    <span className="icon">
-      <FontAwesomeIcon icon={faFacebookSquare} />
-    </span>
-    <span className="icon">
-      <FontAwesomeIcon icon={faInstagram} />
-    </span>
-    <span className="icon">
-      <FontAwesomeIcon icon={faTwitter} />
-    </span>
-  </div>
-);
+const SocialMedia = (props) => {
+  const { size, margin } = props;
+
+  return (
+    <div className="social-media">
+      <span className="icon" style={{ fontSize: size, marginRight: margin }}>
+        <FontAwesomeIcon icon={faFacebookSquare} />
+      </span>
+      <span className="icon" style={{ fontSize: size, marginRight: margin }}>
+        <FontAwesomeIcon icon={faInstagram} />
+      </span>
+      <span className="icon" style={{ fontSize: size, marginRight: margin }}>
+        <FontAwesomeIcon icon={faTwitter} />
+      </span>
+    </div>
+  )
+};
+
+SocialMedia.propTypes = {
+  size: PropTypes.string,
+  margin: PropTypes.string,
+};
+
+SocialMedia.defaultProps = {
+  size: '24px',
+  margin: '1rem',
+};
 
 export default SocialMedia;
